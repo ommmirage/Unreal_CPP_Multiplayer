bd51d53a219450a3de51f222fd6b7151a221d97a
Mirage Om <ommmirage@gmail.com>
2020-02-14 09:29:00 +0400

    Display Main Menu

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -23,6 +23,14 @@ void UPuzzlePlatformsGameInstance::Init()
 	UE_LOG(LogTemp, Warning, TEXT("Found class %s"), *MenuClass->GetName());
 }
 
+void UPuzzlePlatformsGameInstance::LoadMenu()
+{
+	if (!ensure(MenuClass != nullptr)) return;
+	UUserWidget* Menu = CreateWidget<UUserWidget>(this, MenuClass);
+
+	Menu->AddToViewport();
+}
+
 void UPuzzlePlatformsGameInstance::Host()
 {
 	UEngine* Engine = GetEngine();
